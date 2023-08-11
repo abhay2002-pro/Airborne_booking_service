f4ac34b1ad370ab56a19c5b680d9c94376c4191f
abhay2002-pro <abhayray2002@gmail.com>
2023-08-12 00:42:38 +0530

    Started booking service: used managed transaction

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
 const { InfoController } = require("../../controllers");
+const BookingController = require("./booking");
 
 const router = express.Router();
 
 router.get("/info", InfoController.info);
+
+router.use("/bookings", BookingController);
 
 module.exports = router;
