ece5d87aac432e790aac991c0f151f96230f2c79
abhay2002-pro <abhayray2002@gmail.com>
2023-08-13 23:19:26 +0530

    Added cancel booking logic

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -48,7 +48,7 @@ async function makePayment(data) {
       const bookingTime = new Date(bookingDetails.createdAt);
       const currentTime = new Date();
       if(currentTime - bookingTime > 30000){
-        await bookingRepository.update(data.bookingId, {status: CANCELLED}, transaction);
+        await cancelBooking(data.bookingId);
         throw new AppError(`The booking has expired`, StatusCodes.BAD_REQUEST);
       }
 
@@ -68,6 +68,27 @@ async function makePayment(data) {
         await transaction.rollback()
         throw error
     } 
+}
+
+async function cancelBooking(bookingId) {
+    const transaction = await db.sequelize.transaction();
+    try {
+        const bookingDetails = await bookingRepository.get(bookingId, transaction);
+        if(bookingDetails.status === CANCELLED){
+            await transaction.commit();
+            return true;
+        }
+        await axios.patch(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${bookingDetails.flightId}/seats`, {
+            seats: bookingDetails.noOfSeats,
+            dec: 0
+        })
+        await bookingRepository.update(bookingId, {status: CANCELLED}, transaction);
+        await transaction.commit();
+    }
+    catch(error){
+        await transaction.rollback();
+        throw error;
+    }
 }
 module.exports = {
     createBooking,
